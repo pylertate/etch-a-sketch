65ad19b7973d895c7954b5a26a0b53bb06042d4d
Tyler Pate <tylerpate96@gmail.com>
2021-03-30 22:07:22 -0400

    added buttons to toggle colors and an eraser, also changed styles of buttons on hover

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // box that holds the main sketch area
 const sketchBox = document.querySelector("#sketchBox");
 // this var contains all of the actual boxes in the sketchBox
-const sketch = document.getElementsByClassName("sketch");
+const sketch = document.querySelectorAll(".sketch");
 // range for resizing grid
 const sketchRange = document.querySelector("#sketchRange");
 // reset button
@@ -10,14 +10,20 @@ const resetBtn = document.querySelector("#resetBtn");
 // toggle button
 const toggleBtn = document.querySelector("#toggleBtn");
 
+// eraser button
+const eraserBtn = document.querySelector("#eraserBtn")
+
+let color = "rgb(5 5 87)";
+
 // Functions /////////////////////////////////////////////////
 
 // function that lets you draw on each sketch and adds functionality to the reset button
 let sketchColor = function (sketch) {
-sketch.addEventListener(
-  "mouseover",
-  () => (sketch.style.backgroundColor = "rgb(5 5 87)")
-);
+  sketch.addEventListener(
+    "mouseover",
+    () => (sketch.style.backgroundColor = `${color}`)
+  );
+
   resetBtn.addEventListener("click", () => {
     sketch.style.backgroundColor = "#f4f4f4";
   });
@@ -52,5 +58,28 @@ sketchRange.addEventListener("input", () => {
   let number = event.target.value;
   createGrid(number);
 });
+
+toggleBtn.addEventListener("click", () => {
+  switch(color){
+    case (color = "rgb(5 5 87)"):
+      color = "green";
+      break;
+    case (color = "green"):
+      color = "purple";
+      break;
+    case (color = "purple"):
+      color = "rgb(5 5 87)"
+      break;
+    case (color = "#f4f4f4"):
+      color = "rgb(5 5 87)";
+      break;
+  }
+});
+
+eraserBtn.addEventListener("click", ()=>{
+  color = "#f4f4f4"
+} );
+
+
 
 // add functionality to reset button
